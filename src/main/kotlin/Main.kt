5dcb4957a77f86560f5e6cb78562b7ae18972cee
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-04-15 00:03:40 +0900

    add url parse

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,16 @@
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
+import java.net.*
+import java.util.*
+
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
+        val url = URL("http://example.com:80/docs/books/tutorial/index.html?name=networking&name2=123#DOWNLOADING")
+        val queries = url.query
+        val params = queries.split("&").map{it.split("=")}
+
         val httpAsync = "https://httpbin.org/get".httpGet()
             .responseString { request, response, result ->
                 when (result) {
@@ -19,5 +26,13 @@ object Main {
             }
 
         httpAsync.join()
+    }
+}
+
+fun parseParams(queries: String): List<Pair<String, Optional<String>>> {
+
+    return queries.split("&").map{ query ->
+        val keyVal = query.split("=")
+        keyVal[0] to Optional.of(keyVal[1])
     }
 }
