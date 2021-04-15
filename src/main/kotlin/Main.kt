af1de001bf07628fb7037c70fd063f6e4b69e483
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-04-15 21:24:01 +0900

    totyuu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,19 @@ import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import java.net.*
 import java.util.*
+import java.net.URLDecoder
+
+import java.util.AbstractMap.SimpleImmutableEntry
+
+import java.util.stream.Collectors.mapping
+
+import java.util.LinkedHashMap
+
+import java.util.stream.Collectors
+
+import java.util.Arrays
+import java.util.function.Function
+import java.util.function.Supplier
 
 
 object Main {
@@ -29,10 +42,33 @@ object Main {
     }
 }
 
-fun parseParams(queries: String): List<Pair<String, Optional<String>>> {
-
-    return queries.split("&").map{ query ->
-        val keyVal = query.split("=")
-        keyVal[0] to Optional.of(keyVal[1])
+// TODO change to safety library (api)
+fun splitQuery(url: URL): Map<String?, List<String?>?>? {
+    return if(url.query.isNullOrEmpty()) {
+        Collections.emptyMap()
+    } else {
+        url.query.split("&").map {
+            splitQueryParameter(it)
+        }.groupBy {
+            Function<T, K> { obj: T -> obj.key },
+            Supplier<M> { LinkedHashMap() }, mapping(Function<T, U> { java.util.Map.Entry.value }, toList())
+        }
     }
+//    } else Arrays.stream(url.query.split("&"))
+//        .map { it: String? -> this.splitQueryParameter(it) }
+//        .collect(Collectors.groupingBy(
+//            Function<T, K> { obj: T -> obj.key },
+//            Supplier<M> { LinkedHashMap() }, mapping(Function<T, U> { java.util.Map.Entry.value }, toList())
+//        )
+//        )
+}
+
+fun splitQueryParameter(it: String): SimpleImmutableEntry<String?, String?>? {
+    val idx = it.indexOf("=")
+    val key = if (idx > 0) it.substring(0, idx) else it
+    val value = if (idx > 0 && it.length > idx + 1) it.substring(idx + 1) else null
+    return SimpleImmutableEntry(
+        URLDecoder.decode(key, "UTF-8"),
+        URLDecoder.decode(value, "UTF-8")
+    )
 }
