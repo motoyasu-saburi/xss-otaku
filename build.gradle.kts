aa2a9d6d124a4ce176f7832091afc009e54ccb4d
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-04-19 23:32:49 +0900

    replace jUnit5 to Kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    testImplementation("org.mock-server:mockserver-netty:5.11.2")
+    // implementation("org.junit.jupiter:junit-jupiter:5.4.2")
+    // testImplementation(kotlin("test-junit5"))
+    // testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    // testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 
+    testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
+    testImplementation("io.kotest:kotest-assertions-core:.4.4.3")
+
+    testImplementation("org.mock-server:mockserver-netty:5.11.2")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
 }
 
 tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
