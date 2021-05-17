0d793f5dc07693e99970267c8a6510c7b70ffd48
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-05-17 22:27:17 +0900

    temp

--- src/main/kotlin/entity/Arjun.kt
+++ src/main/kotlin/entity/Arjun.kt
@@ -1,29 +1,41 @@
 package entity
 
 class Arjun {
-    fun arjun() {
 
+    // TODO split to config file (enum file?)
+    private val blindParams = listOf("redirect", "redir", "url", "link", "goto", "debug", "_debug", "test", "get", "index", "src", "source", "file",
+        "frame", "config", "new", "old", "var", "rurl", "return_to", "_return", "returl", "last", "text", "load", "email",
+        "mail", "user", "username", "password", "pass", "passwd", "first_name", "last_name", "back", "href", "ref", "data", "input",
+        "out", "net", "host", "address", "code", "auth", "userid", "auth_token", "token", "error", "keyword", "key", "q", "query", "aid",
+        "bid", "cid", "did", "eid", "fid", "gid", "hid", "iid", "jid", "kid", "lid", "mid", "nid", "oid", "pid", "qid", "rid", "sid",
+        "tid", "uid", "vid", "wid", "xid", "yid", "zid", "cal", "country", "x", "y", "topic", "title", "head", "higher", "lower", "width",
+        "height", "add", "result", "log", "demo", "example", "message")
+
+    fun arjun() {
         val response = "" // TODO Normal Request を送り、 Response の Text を受け取る
 
         val regex = Regex("<input.*?name=\'(.*?)\'.*?>|<input.*?name=\"(.*?)\".*?>'")
         val matches = regex.findAll(response)
         matches.forEach {
-//            try:
-//                foundParam = it[1]
-//                except UnicodeDecodeError:
-//                    continue
-//                logger.good('Heuristics found a potentially valid parameter: %s%s%s. Priortizing it.' % (green, foundParam, end))
-//
-//                if foundParam not in blindParams:
-//                blindParams.insert(0, foundParam)
-//                threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=threadCount)
-//                futures = (threadpool.submit(checky, param, paraNames, url,
-//                    headers, GET, delay, timeout) for param in blindParams)
-//                for i, _ in enumerate(concurrent.futures.as_completed(futures)):
-//                if i + 1 == len(blindParams) or (i + 1) % threadCount == 0:
-//                logger.info('Progress: %i/%i\r' % (i + 1, len(blindParams)))
-//                return paraNames
-//            }
-        }
+            try {
+                val matchValue = it.groupValues
+                val singleQuoteMatch = matchValue[0]
+                val foundParam = matchValue[1]  // TODO change "foundParam" --> "foundParam"
+                // logger.good('Heuristics found a potentially valid parameter: %s%s%s. Priortizing it.' % (green, foundParam, end))
 
+                if (!blindParams.any { blindParam -> foundParam == blindParam }) {
+                    blindParams.insert(0, foundParam)
+                }
+            } //catch {
+
+            //}
+        }
+//        threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=threadCount)
+//        futures = (threadpool.submit(checky, param, paraNames, url, headers, GET, delay, timeout) for param in blindParams)
+//        for i, _ in enumerate(concurrent.futures.as_completed(futures)):
+//        if (i + 1 == len(blindParams) or (i + 1) % threadCount == 0) {
+//            logger.info('Progress: %i/%i\r' % (i + 1, len(blindParams)))
+//        }
+//    return paraNames
+    }
 }
